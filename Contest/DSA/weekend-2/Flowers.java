b290f756b4b5517f22c7b3625a15a5512fac271a
Pranusha Thokala <pranushathokala895@gmail.com>
2022-01-21 21:20:26 +0530

    Update Flowers.java
    The flower shop near his house sells flowers of N types. It is found that the store has Ai flowers of the i-th type. We like odd numbers. Therefore, we have decided that there should be an odd number of flowers of each type in the bouquet, and the total number of flowers in the bouquet should also be odd.
    Determine the maximum number of flowers the bouquet can consist of.
    Input
    The first line contains an integer N — the number of types of flowers that are sold in the store
    The second line contains N integers— the number of flowers of each type
    
    1 <= N <= 100000
    1 <= Ai <= 1000
    Output
    Print one number — the maximum number of flowers the bouquet can consist of.
    
    Sample input
    3
    3 5 8
    
    Sample output
    15
    
    Sample input
    3
    1 1 1
    
    Sample output
    3

--- Contest/DSA/weekend-2/Flowers.java
+++ Contest/DSA/weekend-2/Flowers.java
@@ -1,1 +1,31 @@
 
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+	public static void main (String[] args) {
+                      // Your code here
+					  int n;
+					  Scanner sc = new Scanner(System.in);
+					  n = sc.nextInt();
+					  int ans = 0;
+					  int min=Integer.MAX_VALUE;
+					  for(int i=0;i<n;i++){
+						  int temp;
+						  temp = sc.nextInt();
+						  ans += (temp%2 !=0) ? temp : temp-1;
+						  if(min>temp)
+						  min = temp;
+					  }
+					  if(n%2 != 0)
+					  System.out.println(ans);
+					  else{
+						  if(min%2 != 0)
+						   System.out.println(ans-min);
+						   else
+						    System.out.println(ans - min + 1);
+					  }
+	}
+}
