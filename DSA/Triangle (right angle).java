56edadd574512ad7df1354c5739b3a7458ff7f56
Pranusha Thokala <pranushathokala895@gmail.com>
2022-01-21 13:45:26 +0530

    Triangle (right angle)
    In this task you need to print a right angle triangle of height 5.
    Input
    User Task:
    Since this will be a functional problem, you don't have to worry about input. You just have to complete the function printTriangle().
    Output
    Print the right angle triangle of height 5 as shown

---
+++ DSA/Triangle (right angle).java
@@ -0,0 +1,15 @@
+public static void printTriangle(){
+
+ 
+    { 
+        int i, j;  
+        for(i=0; i<5; i++)  
+          {
+            for(j=0; j<=i; j++) 
+            {       
+                System.out.print("* "); 
+            }           
+            System.out.println();
+        } 
+    }
+}
