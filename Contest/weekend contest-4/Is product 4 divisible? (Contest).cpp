c8dae0df5f0162701e107fc5b640ef31c12a06f8
Pranusha Thokala <pranushathokala895@gmail.com>
2022-02-18 14:28:38 +0530

    Create Is product 4 divisible? (Contest).cpp
    You are N integers A[1], A[2],. , A[N]. You need to find whether their product is divisible by 4.
    Input
    The first line of input contains a single integer N.
    The next line contains N singly spaced integers, A[1], A[2],. , A[N].
    
    Constraints
    1 <= N <= 10
    1 <= A[i] <= 1000000000
    Output
    Output "YES" if the product is divisible by 4, else output "NO". (without quotes)
    Sample Input
    5
    3 5 2 5 1
    
    Sample Output
    NO
    
    Explanation: Product = 150 and 150 is not divisible by 4.
    
    Sample Input
    4
    1 3 8 2
    
    Sample Output
    YES
    
    Explanation: Product = 48 and 48 is divisible by 4.

---
+++ Contest/weekend contest-4/Is product 4 divisible? (Contest).cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+
+	// Your code here
+	int n;
+		cin>>n;
+
+		int arr[n];
+
+		for(int i=0;i<n;i++){
+			cin>>arr[i];
+		}
+
+		int p=1;
+
+		for(int i=0;i<n;i++){
+			p*=arr[i];
+		}
+
+		if(p%4==0){
+			cout<<"YES";
+		}else{
+			cout<<"NO";
+		}
+	return 0;
+}
